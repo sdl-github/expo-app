eb5dbfcd0210a5881238f71a300994ed834cf153
sdl-code <sdl-life@foxmail.com>
2024-02-06 23:59:48 +0800

    init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Colors,
+  Dialog,
+  View,
+  PanningProvider,
+  Card,
+  CardProps,
+  Button,
+  Text,
+} from "react-native-ui-lib";
+
+import "./global.css";
+import { useState } from "react";
 
 export default function App() {
+  const [isVisible, setIsVisible] = useState(true)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center ">
       <StatusBar style="auto" />
+      <Text>空空如也</Text>
+      <Button
+        className="mt-5"
+        label={"新增服务器"}
+        size={Button.sizes.medium}
+        backgroundColor={Colors.red30}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
